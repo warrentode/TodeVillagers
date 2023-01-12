cb18752b7f5709b2d533772784cf5294f4922d34
toadie-odie <sabakiyang@yahoo.com>
2023-01-12 03:40:34 -0500

    Fixed the event class, putting it where it actually belongs. Moved the trades to its own class, per profession.

---
+++ src/main/java/net/warrentode/todevillagers/event/ModEvents.java
@@ -0,0 +1,10 @@
+package net.warrentode.todevillagers.event;
+
+
+import net.minecraftforge.fml.common.Mod;
+import net.warrentode.todevillagers.TodeVillagers;
+
+@Mod.EventBusSubscriber(modid = TodeVillagers.MODID)
+public class ModEvents {
+
+}
