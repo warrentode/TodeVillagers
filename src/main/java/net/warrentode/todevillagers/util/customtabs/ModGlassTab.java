2d489225b781eba24c55ac6ff4df3ee4f8baacb3
toadie-odie <sabakiyang@yahoo.com>
2023-02-03 14:28:36 -0500

    reorganized the glassblowing creative tab, including vanilla glass to the list
    Signed-off-by: toadie-odie <sabakiyang@yahoo.com>

--- src/main/java/net/warrentode/todevillagers/util/customtabs/ModGlassTab.java
+++ src/main/java/net/warrentode/todevillagers/util/customtabs/ModGlassTab.java
@@ -3,6 +3,7 @@ package net.warrentode.todevillagers.util.customtabs;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.warrentode.todevillagers.blocks.ModBlocks;
 import net.warrentode.todevillagers.items.ModItems;
@@ -22,55 +23,104 @@ public class ModGlassTab extends CreativeModeTab {
 
     @Override
     public void fillItemList(final @NotNull NonNullList<ItemStack> items) {
+        items.add((ModBlocks.GLASS_KILN.get().asItem().getDefaultInstance()));
         items.add(ModItems.GLASSBLOWER_SHEARS.get().getDefaultInstance());
         items.add(ModItems.GLASSBLOWING_PIPE.get().getDefaultInstance());
         items.add(ModItems.MARVER.get().getDefaultInstance());
         items.add(ModItems.CRUSHED_GLASS.get().getDefaultInstance());
-        items.add((ModBlocks.GLASS_KILN.get().asItem().getDefaultInstance()));
+
+        items.add(Items.GLASS_BOTTLE.getDefaultInstance());
+
         items.add((ModBlocks.REDSTONE_INFUSED_GLASS.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.REINFORCED_GLASS.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_PANE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_STAIRS.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_SLAB.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_WALL.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_FENCE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_FENCE_GATE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_BUTTON.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_PRESSURE_PLATE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_DOOR.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.REINFORCED_GLASS_TRAPDOOR.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.GLOWING_GLASS.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_PANE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_STAIRS.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_SLAB.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_WALL.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_FENCE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_FENCE_GATE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_BUTTON.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_PRESSURE_PLATE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_DOOR.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.GLOWING_GLASS_TRAPDOOR.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.SHIFTING_GLASS.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_PANE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_STAIRS.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_SLAB.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_WALL.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_FENCE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_FENCE_GATE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_BUTTON.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_PRESSURE_PLATE.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_DOOR.get().asItem().getDefaultInstance()));
-        items.add((ModBlocks.SHIFTING_GLASS_TRAPDOOR.get().asItem().getDefaultInstance()));
         items.add((Blocks.TINTED_GLASS.asItem().getDefaultInstance()));
+
+        items.add((Blocks.GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.WHITE_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.ORANGE_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.MAGENTA_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.LIGHT_BLUE_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.YELLOW_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.LIME_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.PINK_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.GRAY_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.LIGHT_GRAY_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.CYAN_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.PURPLE_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.BLUE_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.BROWN_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.GREEN_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.RED_STAINED_GLASS.asItem().getDefaultInstance()));
+        items.add((Blocks.BLACK_STAINED_GLASS.asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_PANE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_PANE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_PANE.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_PANE.get().asItem().getDefaultInstance()));
+
+        items.add((Blocks.GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.WHITE_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.ORANGE_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.MAGENTA_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.LIGHT_BLUE_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.YELLOW_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.LIME_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.PINK_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.GRAY_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.LIGHT_GRAY_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.CYAN_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.PURPLE_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.BLUE_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.BROWN_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.GREEN_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.RED_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+        items.add((Blocks.BLACK_STAINED_GLASS_PANE.asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_STAIRS.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_STAIRS.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_STAIRS.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_STAIRS.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_SLAB.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_SLAB.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_SLAB.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_SLAB.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_WALL.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_WALL.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_WALL.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_WALL.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_FENCE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_FENCE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_FENCE.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_FENCE.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_FENCE_GATE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_FENCE_GATE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_FENCE_GATE.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_FENCE_GATE.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_BUTTON.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_BUTTON.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_BUTTON.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_BUTTON.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_PRESSURE_PLATE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_PRESSURE_PLATE.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_PRESSURE_PLATE.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_PRESSURE_PLATE.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_DOOR.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_DOOR.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_DOOR.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_DOOR.get().asItem().getDefaultInstance()));
+
+        items.add((ModBlocks.REINFORCED_GLASS_TRAPDOOR.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.GLOWING_GLASS_TRAPDOOR.get().asItem().getDefaultInstance()));
+        items.add((ModBlocks.SHIFTING_GLASS_TRAPDOOR.get().asItem().getDefaultInstance()));
         items.add((ModBlocks.TINTED_GLASS_TRAPDOOR.get().asItem().getDefaultInstance()));
     }
 }
